9492538456e381f0cc9a811bd29302dd699cd12e
Ostap <Ostapkriven13@gmail.com>
2023-08-17 13:09:58 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const API_KEY = '38659360-bc0842e55c2c51de5ea7c36c0';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export async function getGallery(word, page) {
     try {
-        const response = await axios.get('https://pixabay.com/api/', {
+        const response = await axios.get(BASE_URL, {
             params: {
                 key: API_KEY,
                 q: word,
@@ -21,3 +22,16 @@ export async function getGallery(word, page) {
         throw error;
     }
 }
+
+
+(async () => {
+    try {
+        const word = 'landscape';
+        const page = 1;
+        
+        const galleryData = await getGallery(word, page);
+        console.log(galleryData);
+    } catch (error) {
+        console.error('Error fetching gallery:', error);
+    }
+})();
