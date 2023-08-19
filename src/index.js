98a08299199d76361d2bea27a01be934dcefede1
Ostap <Ostapkriven13@gmail.com>
2023-08-19 17:22:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,73 +3,76 @@ import { notifyInit } from './notify';
 import { getGallery } from './axios';
 import { createMarkup } from './morkup';
 
-const formSearch = document.querySelector('.search-form');
-formSearch.addEventListener('submit', handleForm);
-
-const btnLoadMore = document.querySelector('.load-more');
-btnLoadMore.addEventListener('click', onLoad);
-
-const gallery = document.querySelector('.gallery');
-
-let word = '';
-let perPage = 40;
-let page = 1;
-let totalPages = 0;
-
-function handleForm(evt) {
-    evt.preventDefault();
-    resetGallery();
-
-    const data = new FormData(evt.currentTarget);
-    word = data.get("searchQuery").trim();
-
-    if (word !== '') {
-        loadGallery(word, page);
-    } else {
-        Notify.warning('Please enter a search query.', notifyInit);
+class GalleryApp {
+    constructor() {
+        this.formSearch = document.querySelector('.search-form');
+        this.btnLoadMore = document.querySelector('.load-more');
+        this.gallery = document.querySelector('.gallery');
+        
+        this.word = '';
+        this.perPage = 40;
+        this.page = 1;
+        this.totalPages = 0;
+        
+        this.formSearch.addEventListener('submit', this.handleForm.bind(this));
+        this.btnLoadMore.addEventListener('click', this.onLoad.bind(this));
+        
+        this.resetGallery();
     }
-}
+    
+    handleForm(evt) {
+        evt.preventDefault();
+        this.resetGallery();
 
-function resetGallery() {
-    gallery.innerHTML = '';
-    page = 1;
-    totalPages = 0;
-    document.querySelector('.footer').classList.remove('open');
-    document.querySelector('.message').textContent = '';
-    btnLoadMore.style.opacity = 0;
-}
+        const data = new FormData(evt.currentTarget);
+        this.word = data.get("searchQuery").trim();
 
-async function loadGallery(searchWord, pageNumber) {
-    try {
-        const data = await getGallery(searchWord, pageNumber);
-        totalPages = Math.ceil(data.totalHits / perPage);
-
-        if (page === 1) {
-            document.querySelector('.footer').classList.remove('open');
-            btnLoadMore.style.opacity = 0;
-        }
-
-        const arr = data.hits;
-        createMarkup(arr);
-
-        if (page < totalPages) {
-            document.querySelector('.footer').classList.add('open');
-            btnLoadMore.style.opacity = 1;
-            document.querySelector('.message').textContent = `Hooray! We found ${data.totalHits} images.`;
+        if (this.word !== '') {
+            this.loadGallery(this.word, this.page);
         } else {
-            document.querySelector('.footer').classList.remove('open');
-            btnLoadMore.style.opacity = 0;
+            Notify.warning('Please enter a search query.', notifyInit);
         }
-    } catch (error) {
-        console.log(error);
+    }
+    
+    resetGallery() {
+        this.gallery.innerHTML = '';
+        this.page = 1;
+        this.totalPages = 0;
+        document.querySelector('.footer').classList.remove('open');
+        document.querySelector('.message').textContent = '';
+        this.btnLoadMore.style.opacity = 0;
+    }
+
+    async loadGallery(searchWord, pageNumber) {
+        try {
+            const data = await getGallery(searchWord, pageNumber);
+            this.totalPages = Math.ceil(data.totalHits / this.perPage);
+
+            if (this.page === 1) {
+                document.querySelector('.footer').classList.remove('open');
+                this.btnLoadMore.style.opacity = 0;
+            }
+
+            const arr = data.hits;
+            createMarkup(arr);
+
+            if (this.page < this.totalPages) {
+                document.querySelector('.footer').classList.add('open');
+                this.btnLoadMore.style.opacity = 1;
+                document.querySelector('.message').textContent = `Hooray! We found ${data.totalHits} images.`;
+            } else {
+                document.querySelector('.footer').classList.remove('open');
+                this.btnLoadMore.style.opacity = 0;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    onLoad() {
+        this.page += 1;
+        this.loadGallery(this.word, this.page);
     }
 }
 
-function onLoad() {
-    page += 1;
-    loadGallery(word, page);
-}
-
-
-
-resetGallery();
+const galleryApp = new GalleryApp();
