9f41922e374d465d3e74bb83974f56ab1c3ce3b0
Ostap <Ostapkriven13@gmail.com>
2023-08-20 21:57:42 +0300

    ss
    s

--- src/axios.js
+++ src/axios.js
@@ -1,35 +1,41 @@
 import axios from 'axios';
 
-const API_KEY = '38659360-bc0842e55c2c51de5ea7c36c0';
-const BASE_URL = 'https://pixabay.com/api/';
+class PixabayGallery {
+    constructor(apiKey) {
+        this.API_KEY = apiKey;
+        this.BASE_URL = 'https://pixabay.com/api/';
+    }
 
-export async function getGallery(word, page) {
-    try {
-        const response = await axios.get(BASE_URL, {
-            params: {
-                key: API_KEY,
-                q: word,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: true,
-                page: page,
-                per_page: 40,
-            }
-        });
+    async getGallery(word, page) {
+        try {
+            const response = await axios.get(this.BASE_URL, {
+                params: {
+                    key: this.API_KEY,
+                    q: word,
+                    image_type: 'photo',
+                    orientation: 'horizontal',
+                    safesearch: true,
+                    page: page,
+                    per_page: 40,
+                }
+            });
 
-        return response.data;
-    } catch (error) {
-        throw error;
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
-
 (async () => {
     try {
+        const apiKey = '38659360-bc0842e55c2c51de5ea7c36c0';
+        const pixabayGallery = new PixabayGallery(apiKey);
+
         const word = 'landscape';
         const page = 1;
         
-        const galleryData = await getGallery(word, page);
+        const galleryData = await pixabayGallery.getGallery(word, page);
         console.log(galleryData);
     } catch (error) {
         console.error('Error fetching gallery:', error);
